519aeef370693b49999a0e395b4a80bb87329930
nagiyan45 <131468227+nagiyan45@users.noreply.github.com>
2023-11-22 16:03:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function updateClock() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
-    setTimeout(updateClock, 1000);
-}
-document.getElementById('fullscreenButton').addEventListener('click', toggleFullscreen);
-
+// 全画面表示のトグル機能
 function toggleFullscreen() {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
@@ -20,5 +9,20 @@ function toggleFullscreen() {
   }
 }
 
+// 時計を更新する機能
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+    setTimeout(updateClock, 1000);
+}
 
-updateClock();
+// ページが読み込まれた後にイベントリスナーを設定
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('fullscreenButton').addEventListener('click', toggleFullscreen);
+    updateClock(); // 時計を起動
+});
