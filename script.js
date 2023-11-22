b40ee06c25aaf7c6786535fb70c2d3e8f0e4b00a
nagiyan45 <131468227+nagiyan45@users.noreply.github.com>
2023-11-22 16:29:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-// 全画面表示のトグル機能
 function toggleFullscreen() {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
+    document.getElementById('fullscreenButton').classList.add('hidden'); // フルスクリーン時にボタンを隠す
   } else {
     if (document.exitFullscreen) {
       document.exitFullscreen();
+      document.getElementById('fullscreenButton').classList.remove('hidden'); // フルスクリーン解除時にボタンを表示
     }
   }
 }
+
+document.addEventListener('fullscreenchange', (event) => {
+  // フルスクリーンが解除されたときにボタンを表示する
+  if (!document.fullscreenElement) {
+    document.getElementById('fullscreenButton').classList.remove('hidden');
+  }
+});
+
 
 // 時計を更新する機能
 function updateClock() {
