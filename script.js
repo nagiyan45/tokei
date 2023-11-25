767f62a3662ff01bc3867609e8fad1074218e657
nagiyan45 <131468227+nagiyan45@users.noreply.github.com>
2023-11-25 13:53:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,19 @@ document.addEventListener('fullscreenchange', (event) => {
   }
 });
 
+// 時計の文字色を更新する機能
+function updateClockColor() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var clockElement = document.getElementById('clock');
 
+    if (hours >= 22 && minutes >= 30 || hours < 7) {
+        clockElement.style.color = 'red'; // 22:30を過ぎたら、または7:00前なら赤色に
+    } else {
+        clockElement.style.color = 'yellow'; // それ以外の時間は黄色に
+    }
+}
 // 時計を更新する機能
 function updateClock() {
     var now = new Date();
@@ -27,6 +39,7 @@ function updateClock() {
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
     document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+    updateClockColor(); // 時計の文字色を更新
     setTimeout(updateClock, 1000);
 }
 
