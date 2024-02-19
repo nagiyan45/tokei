9301d8aca6a1c9801b87a952e1b726586138b8ad
nagiyan45 <131468227+nagiyan45@users.noreply.github.com>
2024-02-19 23:58:24 +0900

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // フォームのデフォルト送信動作を防止
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // 特定のユーザー名とパスワードをチェック
+    if (username === "110higashiurawa" && password === "11541154g") {
+        window.location.href = 'success.html'; // リダイレクト先のページ
+    } else {
+        alert('ユーザー名またはパスワードが間違っています。');
+    }
+});
